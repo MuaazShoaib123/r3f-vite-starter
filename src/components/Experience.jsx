41860fa8aeb4d8e322394b0f3f982097c56e214a
MuaazShoaib123 <muaazrt@gmail.com>
2025-01-07 21:42:56 +0500

    3d avatar with animations

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,28 @@
-import { OrbitControls } from "@react-three/drei";
+
+// My 3d avatar repository 
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from 'leva';
 
 export const Experience = () => {
+
+  const {animation} = useControls({
+    
+    animation: {
+      value :"Standing",
+      options: ["Standing","StandingPose","Pointing","StandingArguing","Talking","Talkingf","Talking3","WalkCircle","WalkLeft","Walking","Yelling"],
+    },
+   
+  });
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Sky/>
+      <Environment preset="sunset"/>
+       <group position-y={-1}>
+       <Avatar animation = {animation} rotation={[0, 0, 1]}/>
+       </group>
+       
     </>
   );
 };
